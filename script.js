0a337d30af6bf293d7f4aec4d2515b64e2280eaf
AdrianHernandez-Dev <a.a.hernandez00@gmail.com>
2019-11-25 20:00:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//
+const apiKey = '3tCrdhVBZWYZTN8bfLl3cPJZ2Yd3oof66eukRWo36yrktz7QiF';
+const searchURL = 'https://api.petfinder.com/v2/animals/';
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function getAnimal(location, type) {
+  const params = {
+    key: apiKey,
+    location: location,
+    type: type
+  };
+  console.log(params);
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+  console.log(url);
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => formatQueryParams(params));
+}
+
+function watchForm() {
+  $('form').submit(function (event) {
+    event.preventDefault();
+    console.log('checked');
+    const location = $('#js-search-term').val();
+    console.log(location);
+    const type = $('#js-search-type').val();
+    console.log(type);
+    getAnimal(location, type);
+  })
+}
+
+$(watchForm);
