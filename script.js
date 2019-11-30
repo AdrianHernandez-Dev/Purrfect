4f3da634f783b007be61603027d92d97beced45d
AdrianHernandez-Dev <a.a.hernandez00@gmail.com>
2019-11-30 14:14:52 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const apiKey = '3tCrdhVBZWYZTN8bfLl3cPJZ2Yd3oof66eukRWo36yrktz7QiF';
 const secret = 'IZAG2o6QDVal1fONI6IXbGqWoAtu6mR69amQZTi6';
-let access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImY3OWRhMmNiNzIyODYzOThjZWEzYjI2MjBlM2ExYjI3Mzc4YjZlYzJjNmNhZmY4OTk3MDQ4ODc4ZGQxOWViNDA4NTVhNTYyNjYwMzA1MTcwIn0.eyJhdWQiOiIzdENyZGhWQlpXWVpUTjhiZkxsM2NQSloyWWQzb29mNjZldWtSV28zNnlya3R6N1FpRiIsImp0aSI6ImY3OWRhMmNiNzIyODYzOThjZWEzYjI2MjBlM2ExYjI3Mzc4YjZlYzJjNmNhZmY4OTk3MDQ4ODc4ZGQxOWViNDA4NTVhNTYyNjYwMzA1MTcwIiwiaWF0IjoxNTc1MTMyNDY1LCJuYmYiOjE1NzUxMzI0NjUsImV4cCI6MTU3NTEzNjA2NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.iupW4ntWSrU4vHc0eYP0WltSmER3QHxEQxuBTWG1btAZITkMFUA921DDnUFoGOU3Ij6zbUhO4bRQ5TvqHIGHZau-VoXcdtge5XGD7apVNw_b8IZOKfj6Xebkh61cqsX6YeaOgLxu1EXMKJWWOa0iDkUa17su0GteOW1LaFcbxqp2W_mS_fIefBk5YXHiBzssZBnO_vqROJagvxE77yWi3dqAs6SkFcckDeoUhetMTzDEeFPp6rjf51iRhFWTS_izBKwA0WhZjeuaeQYaJ1HfCErjWZr-AOxbpojGdekiXjRR2HzQotqjcdDDhPTs8BoIRsOxoyoY2O9MRXO8VWPqEQ';
+let access_token = ' ';
 const searchURL = 'https://api.petfinder.com/v2/animals/';
 const accessURL = 'https://api.petfinder.com/v2/oauth2/token'
+let expires = ' '
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -21,7 +22,6 @@ function displayResults(responseJson) {
       console.log(i);
       $('#results-list').append(`
  <li> <h3>${responseJson.animals[i].name}</h3>
- <img src="${responseJson.animals[i].photos.medium}">
  <p>Breed: ${responseJson.animals[i].breeds.primary}</p>
  <p>Age: ${responseJson.animals[i].age}</p>
  <p>Location: ${responseJson.animals[i].contact.address.city}</p>
@@ -29,23 +29,8 @@ function displayResults(responseJson) {
  <p>Email: ${responseJson.animals[i].contact.email}</p>
  <a href="${responseJson.animals[i].url}">Find out more</a>`)
     };
-    //console.log(distance);
   }
 }
-
-function getAccess(){
-  const params = {
-    grant_type : 'client_credentials',
-    client_id : apiKey,
-    client_secret : secret
-  };
-  console.log(params);
-  const queryString = formatQueryParams(params)
-  const url = accessURL + '?' + queryString;
-  console.log(url);
-  $.post(url, )
-}
-
 
 function getAnimal(location, type) {
   const params = {
@@ -58,9 +43,8 @@ function getAnimal(location, type) {
   console.log(url);
   fetch(url, {
     headers: {
-      'Authorization': 'Bearer ' + access_token
+      'Authorization': 'Bearer ' +  access_token
     },
-    body: JSON.stringify()
   })
     .then(response => {
       if (response.ok) {
@@ -82,14 +66,42 @@ function watchForm() {
     console.log(location);
     const type = $('#js-search-type').val();
     console.log(type);
-    getAccess();
+    //getAccess();
     getAnimal(location, type);
   })
 }
 
+let auth = function (){
+  fetch(`https://api.petfinder.com/v2/oauth2/token`, {
+  method: 'POST',
+  body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+})
+.then(res => res.json())
+.then(function (json){
+  console.log(json);
+  access_token = json.access_token;
+  console.log(access_token);
+  //so i have the token here and I fell like i need to push this to the global variable.
+  expires = json.expires_in;
+  console.log(expires);
+});
+}
+
+function checkToken(){
+  if(expires < 0){
+    auth();
+  }
+  else {
+    watchForm();
+  }
+  }
+
 function handleSearch(){
-  //getAccess();
-  watchForm();
+  auth();
+  checkToken();
 }
 
 $(handleSearch);
